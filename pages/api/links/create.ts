e03123485c41ca1ca7b4689b5caa029ce701027c
pace11 <ryanjoker87@gmail.com>
2025-02-27 05:59:32 +0700

    feat: init app

---
+++ pages/api/links/create.ts
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '@/lib/db'
+import { linksTable } from '@/lib/db/schema'
+
+type Response = {
+  insertedId?: number
+  message?: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{ data: Response[] }>,
+) {
+  if (req.method !== 'POST') {
+    res.status(405).json({ data: [{ message: 'Method not allowed' }] })
+  }
+
+  const payload = JSON.parse(req.body)
+
+  const data = await db
+    .insert(linksTable)
+    .values(payload)
+    .returning({ insertedId: linksTable.id })
+
+  res.status(200).json({ data })
+}
